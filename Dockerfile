842b1da7cf67c1e262593acf472a609fad0b07b4
Praful M <prafulchandra0123@gmail.com>
2025-05-10 00:32:47 +0530

    Added Dockerfile and build settings with a sample main.py

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
-FROM python3.11
+FROM python:3.13-alpine
+
 LABEL maintainer="Praful M"
 
+WORKDIR /app
+
+COPY . .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8000
+
+CMD ["python3", "app/main.py"]
---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os 
+
+app = FastAPI()
+
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
