e8fa2cbb8863015a5c40f1b0e161b3d07c7e8bf2
Praful M <prafulchandra0123@gmail.com>
2025-05-10 15:16:53 +0530

    Port Confusion changed from 8000 to 8001

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn -w 4 -b 0.0.0.0:8000 app.main:app --timeout 120 --reload --workers 1 --worker-class gthread --threads 4
+gunicorn -w 4 -b 0.0.0.0:8001 app.main:app --timeout 120 --reload --workers 1 --worker-class gthread --threads 4
